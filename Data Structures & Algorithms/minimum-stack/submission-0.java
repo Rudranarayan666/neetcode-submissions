986ba6269a4959ec947c0244a092fe06e6b90041
Rudranarayan Sahu <23104070@apsit.edu.in>
2026-09-18 22:08:51 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,34 @@
+class MinStack {
+    private int[] stack;
+    private int[] minStack;
+    private int top;
+
+    public MinStack() {
+        stack = new int[30001];
+        minStack = new int[30001];
+        top = -1;
+    }
+    
+    public void push(int val) {
+        top++;
+        stack[top] = val;
+
+        if (top == 0) {
+            minStack[top] = val;
+        } else {
+            minStack[top] = Math.min(val, minStack[top - 1]);
+        }
+    }
+    
+    public void pop() {
+        top--;
+    }
+    
+    public int top() {
+        return stack[top];
+    }
+    
+    public int getMin() {
+        return minStack[top];
+    }
+}
